eb20203ab8e79b43d791f6132144ddb1884b0cfd
Arnob Rahman <arnobr99@gmail.com>
2023-02-19 19:48:15 +0600

    one_time_pad

---
+++ one_time_pad/Decryption.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void Decrypt(char cipher[], char key[])
+{
+
+    int len = strlen(cipher);
+    char message[1000];
+
+    for (int i = 0; i < len; i++)
+    {
+        message[i] = ((cipher[i] - 'A') ^ (key[i] - 'A')) + 'A';
+    }
+    message[len] = '\0';
+
+    printf("The decrypted message is: %s\n", message);
+}
+
+
+
+int main()
+{
+    char cipher[1000], key[1000];
+
+    printf("Enter the encrypted message: ");
+    scanf("%s", cipher);
+    printf("Enter the key to decrypt: ");
+    scanf("%s", key);
+
+    Decrypt(cipher, key);
+
+    return 0;
+}
---
+++ one_time_pad/Encryption.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void Encrypt(char message[], char key[])
+{
+
+    int len = strlen(message);
+    char cipher[1000];
+
+    for (int i = 0; i < len; i++)
+    {
+        cipher[i] = ((message[i] - 'A') ^ (key[i] - 'A')) + 'A';
+    }
+    cipher[len] = '\0';
+
+    printf("The encrypted message is: %s\n", cipher);
+}
+
+int main()
+{
+    char message[1000], key[1000];
+
+    printf("Enter the message: ");
+    scanf("%s", message);
+    printf("Enter the key encrypt: ");
+    scanf("%s", key);
+
+    Encrypt(message, key);
+
+    return 0;
+}
